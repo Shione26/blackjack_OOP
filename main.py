4163f578e10c063a3cf464fa9a0a133d7bd4b2a4
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-06-25 00:52:34 +0800

    created exit program

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from game_class import Game
 
 def main():
-    game = Game()
-    game.start()
+    while True:
+        game = Game()
+        game.start()
+
+        # exit program
+        new_game = input("\nPlay another hand? [Y/N] ")
+        while new_game.lower() not in ["y", "n"]:
+            new_game = input("Invalid Input. Please enter 'y' or 'n' ")
+
+        if new_game.lower() != "y":
+            print("\n------------------------Thanks for playing!---------------------\n")
+            break
 
 if __name__ == "__main__":
     main()
