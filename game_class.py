676af2f4dd82e6f7b4b209c76c5cb5028e1a3f49
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-06-24 23:56:30 +0800

    created game class and added game welcome screen

---
+++ game_class.py
@@ -0,0 +1,14 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def start(self):
+        print("\n----------------------------------------------------------------")
+        print("                ♠♣♥♦ WELCOME TO BLACKJACK! ♠♣♥♦")
+        print("                          Lets Play!")
+        print("----------------------------------------------------------------")
+        print("""
+Game Rules:  Get as close to 21 as you can without going over!
+        Dealer hits until he/she reaches 17.
+        Aces count as 1 or 11.
+        """)    
---
+++ main.py
@@ -0,0 +1,8 @@
+from game_class import Game
+
+def main():
+    game = Game()
+    game.start()
+
+if __name__ == "__main__":
+    main()
