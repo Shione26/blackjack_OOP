9c5210b1c16b3a11e8716cc3a342f91c72c53a27
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-06-24 22:28:01 +0800

    created player and dealer classes inheriting from person class

--- person_class.py
+++ person_class.py
@@ -9,3 +9,11 @@ class Person:
         card = deck.deal()          # take a card from the deck
         self.hand.add_card(card)    # add it to the person's hand
         return card
+    
+class Player(Person):
+    def __init__ (self, name="Player"):
+        super().__init__ (name)
+
+class Dealer(Person):
+    def __init__ (self, name="Dealer"):
+        super().__init__ (name)
