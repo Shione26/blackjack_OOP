097362d9bc22bff214f2245700042a9d1adf9dbb
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-06-24 19:49:39 +0800

    added methods to card class

--- card_class.py
+++ card_class.py
@@ -5,3 +5,9 @@ class Card:
     def __init__(self, suit, rank):
         self._suit = suit
         self._rank = rank
+
+    def __str__(self):
+        return f"{self._rank} of {self._suit}"
+
+    def get_rank(self):
+        return self._rank
