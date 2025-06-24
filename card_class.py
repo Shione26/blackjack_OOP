75c6912d6800126ee97e3f3a1a782f43073afce1
Shane Crisostomo <shaneleannecrisostomo@gmail.com>
2025-06-24 20:00:35 +0800

    define card values dictionary and implement get_value method

--- card_class.py
+++ card_class.py
@@ -1,5 +1,20 @@
 suits = ("Spades ♠", "Clubs ♣", "Hearts ♥", "Diamonds ♦")
 ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+values = {
+    "2": 2, 
+    "3": 3, 
+    "4": 4, 
+    "5": 5,
+    "6": 6, 
+    "7": 7, 
+    "8": 8, 
+    "9": 9,
+    "10": 10, 
+    "J": 10, 
+    "Q": 10, 
+    "K": 10, 
+    "A": 11
+}
 
 class Card:
     def __init__(self, suit, rank):
@@ -11,3 +26,6 @@ class Card:
 
     def get_rank(self):
         return self._rank
+    
+    def get_value(self):
+        return values[self._rank]
